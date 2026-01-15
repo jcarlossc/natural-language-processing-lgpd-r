95d9eedbe64c49e10e40b7b795b5ff2629663c75
jcarlossc <josecscosta@fac.pe.senac.br>
2026-01-15 15:58:41 -0300

    Arquivo de configurações

---
+++ config/config.R
@@ -0,0 +1,10 @@
+# Caminhos do projeto
+PATH_CONFIG <- "config"
+PATH_RAW <- "dados/dados_base"
+PATH_PROCESSED <- "dados/dados_processados"
+PATH_SCRIPTS <- "scripts_R"
+PATH_LOGS <- "logs"
+
+# Configurações 
+ENCODING <- "UTF-8"
+LANGUAGE <- "pt"
