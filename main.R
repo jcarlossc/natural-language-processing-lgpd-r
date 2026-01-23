34e4ef38ecbace4b93580ed5290b4f422ac9110e
jcarlossc <josecscosta1977@gmail.com>
2026-01-23 15:58:56 -0300

    Sctipt clean_text adicionado

--- main.R
+++ main.R
@@ -1,8 +1,13 @@
 source("config/config.R")
 source(file.path(PATH_LOGS, "pipeline_logs.R"))
 source(file.path(PATH_SCRIPTS, "loader_text.R"))
+source(file.path(PATH_SCRIPTS, "clean_text.R"))
 
 file <- "lgpd.txt"
 
-text_loaded <- load_text(file.path(PATH_RAW, file), ENCODING)
+text_loaded <- load_text(file.path(PATH_RAW, file), ENCODING, text_col)
 text_loaded
+
+text_clean <- clean_text(text_loaded, ENCODING, LANGUAGE, text_col)
+text_clean
+
